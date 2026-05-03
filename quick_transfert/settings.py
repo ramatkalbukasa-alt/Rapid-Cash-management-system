fc236895684b1d9d0ad0ab435e78d6e9331a317c
ramatkalbukasa-alt <ramatkalbukasa@gmail.com>
2026-05-03 13:50:55 +0300

    fix: switch to CompressedStaticFilesStorage to avoid manifest errors

--- quick_transfert/settings.py
+++ quick_transfert/settings.py
@@ -145,8 +145,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-WHITENOISE_MANIFEST_STRICT = False
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+# WHITENOISE_MANIFEST_STRICT = False  # Not needed for CompressedStaticFilesStorage
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
