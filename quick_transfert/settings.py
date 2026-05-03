bfc5b86435dc04e28ce9e31ee8f10be43206fc78
ramatkalbukasa-alt <ramatkalbukasa@gmail.com>
2026-05-03 13:59:14 +0300

    fix: add leading slash to STATIC_URL and use absolute paths

--- quick_transfert/settings.py
+++ quick_transfert/settings.py
@@ -142,9 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # WHITENOISE_MANIFEST_STRICT = False  # Not needed for CompressedStaticFilesStorage
 
