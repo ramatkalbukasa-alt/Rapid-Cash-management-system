05206d4c81e41ea30af1c00a6fcbfe5e9d853abb
ramatkalbukasa-alt <ramatkalbukasa@gmail.com>
2026-05-03 12:58:14 +0300

    fix: add *.onrender.com to ALLOWED_HOSTS

--- quick_transfert/settings.py
+++ quick_transfert/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY', default='django-insecure-change-this-in
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default='False', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,*.onrender.com').split(',')
 
 
 # Application definition
